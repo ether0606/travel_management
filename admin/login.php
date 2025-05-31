c31eb74775bfa9a5a63ab7ee1c3655caa4645363
WDPF_IDB <WDPF@nh.ctg>
2025-05-31 10:26:40 +0600

    login page change

--- admin/login.php
+++ admin/login.php
@@ -72,16 +72,13 @@ session_start();
 
               <div class="separator">
                 <p class="change_link">New to site?
-                  <a href="#signup" class="to_register"> Create Account </a>
+                  <a href="#signup" class="to_register"> Create an Account </a>
                 </p>
 
                 <div class="clearfix"></div>
                 <br />
 
-                <div>
-                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 4 template. Privacy and Terms</p>
-                </div>
+                
               </div>
             </form>
           </section>
@@ -138,10 +135,7 @@ session_start();
                 <div class="clearfix"></div>
                 <br />
 
-                <div>
-                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 4 template. Privacy and Terms</p>
-                </div>
+                
               </div>
             </form>
           </section>
