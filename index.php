83a59a54e9a30c2429b8fa8051a4a562af3d015f
Nasif006 <md.nasifiqbal006@gmail.com>
2025-07-01 10:32:49 +0600

    s

--- index.php
+++ index.php
@@ -610,3 +610,11 @@
 	</section><!--/.special-offer end-->
 	<!--special-offer end-->
 <?php include_once('includes/footer.php'); ?>
+<?php
+
+// the message
+	$msg = "Thank you for booking with us. Your booking invoice number is INV-";
+	// send email
+	mail("idbbatch64@gmail.com","Flight Booking",$msg,'info@wdpfctg.site');
+
+	?>
